c7d1d5d26d4053c01587962f0f5a736eee29704e
AndreasNelson <nel02254@morris.umn.edu>
2024-04-26 10:51:04 -0500

    cleaned up testing interface

--- TheracTestEnvironment.java
+++ TheracTestEnvironment.java
@@ -2,7 +2,7 @@ public class TheracTestEnvironment {
 	public static void main(String args[]) {
 		// example of a proper procedure
 		Therac therac = Therac.getInstance();
-		therac.lowerShield(); // lower shield for low beam
+/* 		therac.lowerShield(); // lower shield for low beam
 		therac.fireLowBeam(); // fire low beam to scan for cancer
 		therac.raiseShield(); // raise shield for high beam
 		therac.fireHighBeam(); // fire high beam to treat cancer
@@ -11,7 +11,7 @@ public class TheracTestEnvironment {
 		// attempted misfire
 		Therac therac2 = Therac.getInstance();
 		therac.lowerShield(); // lower shield
-		therac.fireHighBeam(); // fire the high beam without raising the shield
+		therac.fireHighBeam(); // fire the high beam without raising the shield */
 
 		// command line interface for end user
 		for (String arg : args) {
