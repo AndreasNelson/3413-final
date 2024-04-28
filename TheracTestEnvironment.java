7a9b24909dec878cb89c2aab5857c61cca7d6834
AndreasNelson <nel02254@morris.umn.edu>
2024-04-28 16:00:35 -0500

    added safety check to test environment between commands

--- TheracTestEnvironment.java
+++ TheracTestEnvironment.java
@@ -1,29 +1,33 @@
 public class TheracTestEnvironment {
-	public static void main(String[] args) {
-		Therac therac = Therac.getInstance();
+    public static void main(String[] args) {
+        Therac therac = Therac.getInstance();
         
+        System.out.println("Welcome to the Therac-25 test environment");
+        System.out.println("Commands: lowerShield, fireLowBeam, raiseShield, fireHighBeam\n______________________________________________________________________________\n");
 
-		System.out.println("Welcome to the Therac-25 test environment");
-		System.out.println("Commands: lowerShield, fireLowBeam, raiseShield, fireHighBeam\n______________________________________________________________________________\n");
+        for (String command : args) {
+            if (!therac.isSafe()) {
+                System.out.println("The system is not safe. Aborting command: " + command);
+                break;
+            }
 
-		for (String command : args) {
             switch (command.toLowerCase()) {
-				case "lowershield":
-					therac.lowerShield();
-					break;
-				case "firelowbeam":
-					therac.fireLowBeam();
-					break;
-				case "raiseshield":
-					therac.raiseShield();
-					break;
-				case "firehighbeam":
-					therac.fireHighBeam();
-					break;
-				default:
-					System.out.println("Invalid command: " + command);
-					break;
-			}
+                case "lowershield":
+                    therac.lowerShield();
+                    break;
+                case "firelowbeam":
+                    therac.fireLowBeam();
+                    break;
+                case "raiseshield":
+                    therac.raiseShield();
+                    break;
+                case "firehighbeam":
+                    therac.fireHighBeam();
+                    break;
+                default:
+                    System.out.println("Invalid command: " + command);
+                    break;
+            }
         }
-	}
+    }
 }
