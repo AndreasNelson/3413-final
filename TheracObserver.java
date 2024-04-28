c9a7d30f795e0e3098640ce915872e11b6cffcce
AndreasNelson <nel02254@morris.umn.edu>
2024-04-28 15:10:02 -0500

    added beam counting to TheracObserver

--- TheracObserver.java
+++ TheracObserver.java
@@ -1,6 +1,26 @@
 public class TheracObserver implements Observer {
-    @Override
+    private int highBeamCount = 0;
+    private int lowBeamCount = 0;
+    private boolean safe = true;
+
     public void update(String event) {
-        System.out.println("Observers has been notified that " + event + " event has occurred!");
+        switch (event) {
+            case "high intensity beam fired":
+                highBeamCount++;
+                break;
+            case "low intensity beam fired":
+                lowBeamCount++;
+                break;
+            case "high intensity beam fired more than 3 times":
+            case "low intensity beam fired more than 3 times":
+                safe = false;
+                break;
+            default:
+                System.out.println("Received unknown event: " + event);
+        }
+
+        if (!safe) {
+            System.out.println("The system is not safe. High intensity beam fired: " + highBeamCount + " times. Low intensity beam fired: " + lowBeamCount + " times.");
+        }
     }
 }
